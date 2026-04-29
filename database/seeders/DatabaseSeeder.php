7d1984b08664400d5dc45dd4c5f98b5a5dbf7712
ekodrive <ekodrive@gmail.com>
2026-04-29 10:17:06 +0400

    Remove unused User model reference and cleanup seeder logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,18 +9,8 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::updateOrCreate(
-            ['email' => 'test@example.com'],
-            ['name'  => 'Test User'],
-        );
-
         $this->call([
             AdminSeeder::class,
             MessageTemplateSeeder::class,
